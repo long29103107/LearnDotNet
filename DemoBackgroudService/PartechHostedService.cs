082acf0f0f0a95e5f51996451379174672fc5dd9
longnguyen <long.nguyen@siliconstack.com.au>
2022-06-07 15:48:16 +0700

    update code

---
+++ DemoBackgroudService/PartechHostedService.cs
@@ -0,0 +1,23 @@
+﻿namespace DemoBackgroudService
+{
+    public class PartechHostedService : IHostedService
+    {
+        private Timer _timer;
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(HelloWorld, null, 0, 10000);
+            return Task.CompletedTask;
+        }
+
+        void HelloWorld(object state)
+        {
+            Console.WriteLine(DateTime.Now.ToString() + " Welcome to Partech World");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+    }
+}
