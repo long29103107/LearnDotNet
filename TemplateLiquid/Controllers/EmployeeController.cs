e28a8edefa7970aa7406f83518c3d3416db28c93
longnguyen <long.nguyen@siliconstack.com.au>
2022-06-15 17:18:35 +0700

    update code

---
+++ TemplateLiquid/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TemplateLiquid.Models;
+using TemplateLiquid.Repositories;
+
+namespace TemplateLiquid.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public readonly EmployeeRepository repository;
+        public EmployeeController() => repository = new EmployeeRepository();
+        public IActionResult Index()
+        {
+            var employees = repository.GetAll();
+            return View(employees);
+        }
+        public IActionResult Create() => View();
+        [HttpPost]
+        public IActionResult Create([FromForm] Employee employee)
+        {
+            repository.Add(employee);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ TemplateLiquid/Repositories/EmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using TemplateLiquid.Models;
+
+namespace TemplateLiquid.Repositories
+{
+    public class EmployeeRepository
+    {
+        private static Dictionary<int, Employee> employees = new Dictionary<int, Employee>();
+        public List<Employee> GetAll()
+        {
+            return employees.Values.ToList();
+        }
+        public void Add(Employee employee)
+        {
+            employee.Id = employees.Count + 1;
+            employees.Add(employee.Id, employee);
+        }
+    }
+}
